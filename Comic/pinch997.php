09e9731312ca83fc51394aba1c260f333ca678af
PinCh997 <120103078+PinCh997@users.noreply.github.com>
2022-12-15 12:29:50 +0100

    edit html to php

---
+++ Comic/pinch997.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Dokumentation</title>
+</head>
+<body>
+
+<?php
+echo $_SERVER['SERVER_NAME'];
+?>
+
+</body>
+</html>
