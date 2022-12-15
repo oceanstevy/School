91aef3677b3fa4eebb11324e143a8cb61178b0a9
MikaGrof <gromi088@school.lu>
2022-12-15 12:36:16 +0100

    Added Address and Servername

---
+++ Spaceship/Space Engineers Ship Stats.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Space Engineers Ship stats</title>
+</head>
+<body>
+<h1>
+    Space Engineers Ship stats
+</h1>
+    <p>
+        <h2>To get a Ships stats use following URL:</h2>
+        https://89.58.47.144/1TPIF2/WEBAP/API_Collection/Spaceship/stats/[Name]
+</p><br>
+<?php
+    echo $_SERVER['SERVER_NAME'];
+?>
+
+</body>
+</html>
+
