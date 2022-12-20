d0c3dcd35958f12e8632fd46fe9a1ea1d1a75165
OlingerSam <120405316+OlingerSam@users.noreply.github.com>
2022-12-20 13:34:16 +0100

    Create index.php

---
+++ Food/index.php
@@ -0,0 +1,29 @@
+<?php
+    $link = "89.58.47.144/1TPIF2/WEBAP/API_Collection/";
+?>
+
+<h1>Obst</h1>
+<h2><a href="<?php echo $link?>/Food/Obst"><?php echo $link?>/Food/Obst</a></h2>
+<p>Hier sollen ID, Name, Kalorien, Gewicht und Nährstoffe angezeigt werden. Einige Nährstoffe wie z.b. Fett sollen nocheinmal unterteilt werden. Durchschnittlicher Preis.
+</p>
+
+<h1>Gemüse</h1>
+<h2><a href="<?php echo $link?>/Food/Gemüse"><?php echo $link?>/Food/Gemüse</a></h2>
+<p>Hier sollen ID, Name, Kalorien, Gewicht und Nährstoffe angezeigt werden. Einige Nährstoffe wie z.b. Fett sollen nocheinmal unterteilt werden. Durchschnittlicher Preis.
+</p>
+
+<h1>Fleisch</h1>
+<h2><a href="<?php echo $link?>/Food/Fleisch"><?php echo $link?>/Food/Fleisch</a></h2>
+<p>Hier sollen ID, Name, Kalorien, Gewicht und Nährstoffe angezeigt werden. Einige Nährstoffe wie z.b. Fett sollen nocheinmal unterteilt werden. Durchschnittlicher Preis.
+</p>
+
+<h1>Tierprodukte</h1>
+<h2><a href="<?php echo $link?>/Food/Tierprodukte"><?php echo $link?>/Food/Tierprodukte</a></h2>
+<p>Hier sollen ID, Name, Kalorien, Gewicht und Nährstoffe angezeigt werden. Einige Nährstoffe wie z.b. Fett sollen nocheinmal unterteilt werden. Durchschnittlicher Preis.
+</p>
+
+<h1>Gerichte</h1>
+<h2><a href="<?php echo $link?>/Food/Gerichte"><?php echo $link?>/Food/Gerichte</a></h2>
+<p>Hier sollen ID, Name, Kalorien, Gewicht und Nährstoffe angezeigt werden. Einige Nährstoffe wie z.b. Fett sollen nocheinmal unterteilt werden. Durchschnittlicher Preis.
+    Vegetarisch oder nicht. Zutaten
+</p>
