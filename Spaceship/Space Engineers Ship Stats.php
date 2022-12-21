f596bab5616fc0dc27452c89b42d6635c704d1dc
MikaGrof <gromi088@school.lu>
2022-12-21 19:00:02 +0100

    Added CSS and removed Image, added error message incase Address doesnt match up

--- Spaceship/Space Engineers Ship Stats.php
+++ Spaceship/Space Engineers Ship Stats.php
@@ -6,11 +6,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Space Engineers Ship stats</title>
+    <style>
+        body{
+            margin-left: 10%;
+            margin-right: 10%;
+            background-blend-mode: screen;
+            background: linear-gradient(to right, #e6ffe6, #b3e0ff);
+            min-width: 1000px;
+        }
+        #Info{
+            color: dimgrey;
+            font-weight: bolder;
+            font-family: Monocraft;
+            margin: 5% 20% 5% 20%;
+            padding: 10% 5% 10% 5%;
+            text-align: center;
+            backdrop-filter: sepia(35%);
+            border-radius: 25px;
+
+        }
+    </style>
 </head>
 <body>
-<h1>
-    Space Engineers Ship stats
-</h1>
+<div id="Info">
+    <h1>
+        Space Engineers Ship stats
+    </h1>
     <p>
         <h2>To get a Ships stats use following URL:</h2>
 
@@ -25,12 +46,14 @@
             https://89.58.47.144/1TPIF2/WEBAP/API_Collection/Spaceship/Stats/{Name}
             </p><br>
         ";
-    }else
-    echo $_SERVER['SERVER_NAME'];
-    //Test image from steam
-    echo '<img src="https://steamuserimages-a.akamaihd.net/ugc/1783966736434772570/9DC9EAE4E950CA7616ED51E4C4E7F2E1921EDAB1/?imw=1024&imh=576&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true" alt="img">';
+    }else {
+        echo $_SERVER['SERVER_NAME'];
+        //Test image from steam
+        echo "Server Error";
+    }
 ?>
 
+</div>
 </body>
 </html>
 
