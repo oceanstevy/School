4cf9508400a63858e4be482ff91a8469e1e4ae66
TheSa152 <thesa152@school.lu>
2022-12-21 11:02:15 +0100

    Update css, home, navigation

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <header>
+    <h1>Formula 1 API</h1>
  <ul>
-     <li><p>Forumula 1 API</p></li>
   <li><a href="index.php?page=home">Home</a></li>
   <li><a href="index.php?page=api">Api</a></li>
 </ul>
